24758381509f7e69f8a994900cc50004f9d20da3
joshuabezaleel <joshua.bezaleel@gmail.com>
2019-09-17 16:48:16 +0700

    Add user, plain

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+
+	"github.com/joshuabezaleel/library-server/database"
+	"github.com/joshuabezaleel/library-server/pkg/core/user"
+	"github.com/joshuabezaleel/library-server/server"
+)
+
+const (
+	port         = ":8082"
+	connhost     = "localhost"
+	connport     = 8081
+	connusername = "postgres"
+	connpassword = "postgres"
+	dbname       = "test1"
+)
+
+func main() {
+	connectionString := fmt.Sprintf("host = %s port=%d user=%s password=%s dbname=%s sslmode=disable", connhost, connport, connusername, connpassword, dbname)
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Setting up domain repositories
+	var (
+		userRepo user.Repository
+	)
+	userRepo = database.NewRepository(db)
+
+	// Setting up domain services
+	var (
+		userService user.Service
+	)
+	userService = user.NewService(userRepo)
+
+	srv := server.New(userService)
+
+	err = http.ListenAndServe(port, srv.Router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ server/user.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/joshuabezaleel/library-server/pkg/core/user"
+)
+
+type userHandler struct {
+	service user.Service
+}
+
+// func (handler *userHandler) router() *mux.Router {
+// 	root := mux.NewRouter()
+// 	router := root.PathPrefix("/user").Subrouter()
+
+// 	router.HandleFunc("/register", handler.registerUser).Methods("POST")
+
+// 	return root
+// }
+
+func (handler *userHandler) RegisterRouter(router *mux.Router) {
+	router.HandleFunc("/register", handler.registerUser).Methods("POST")
+}
+
+func (handler *userHandler) registerUser(w http.ResponseWriter, r *http.Request) {
+	// ctx := context.Background()
+
+	var request struct {
+		Username string
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer r.Body.Close()
+
+	err = handler.service.RegisterUser(request.Username)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode("New user registered.")
+}
